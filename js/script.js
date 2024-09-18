62aa050c0ab3dda50d43b588e37b65d67ec8b203
Guilherme <borbatovs@gmail.com>
2024-09-18 10:26:26 -0300

    add typewritter effect

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const typewriterElement = document.getElementById('typewriter');
+    const text = "@villelabrasilbank"; // O texto que você quer animar
+    const typingSpeed = 150; // Velocidade de digitação (em milissegundos)
+    const erasingSpeed = 100; // Velocidade de apagamento (em milissegundos)
+    const waitBeforeErasing = 2000; // Tempo de espera antes de apagar (em milissegundos)
+    const waitBeforeRetyping = 1000; // Tempo de espera antes de redigitar (em milissegundos)
     const carousel = document.querySelector('.carousel');
     const cards = carousel.querySelectorAll('.card');
     const pagination = document.querySelector('.pagination');
@@ -7,6 +13,42 @@ document.addEventListener('DOMContentLoaded', function() {
     const totalCards = cards.length;
     let isMobile = window.innerWidth <= 768;
 
+    function typeWriter(text, i, cb) {
+        if (i < text.length) {
+            typewriterElement.innerHTML = text.substring(0, i+1);
+            i++;
+            setTimeout(function() {
+                typeWriter(text, i, cb)
+            }, typingSpeed);
+        } else if (typeof cb == 'function') {
+            setTimeout(cb, waitBeforeErasing);
+        }
+    }
+
+    function eraseText(i, cb) {
+        if (i > 1) { // Mudamos para 1 para manter o primeiro caractere
+            typewriterElement.innerHTML = text.substring(0, i);
+            i--;
+            setTimeout(function() {
+                eraseText(i, cb)
+            }, erasingSpeed);
+        } else if (typeof cb == 'function') {
+            setTimeout(cb, waitBeforeRetyping);
+        }
+    }
+
+    function typeLoop() {
+        typeWriter(text, 0, function() {
+            eraseText(text.length, typeLoop);
+        });
+    }
+
+    // Inicializa com o primeiro caractere
+    typewriterElement.innerHTML = text.charAt(0);
+    
+    // Inicia o loop de animação após um breve delay
+    setTimeout(typeLoop, 1000);
+    
     function updateCarousel(index) {
         if (!isMobile) {
             cards.forEach((card, i) => {
